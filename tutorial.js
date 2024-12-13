06c389e02544c4d3e41f0d3cf2b420d5c87656c3
ro-hitkr <156408369+ro-hitkr@users.noreply.github.com>
2024-12-13 17:02:45 +0530

    Create tutorial.js

---
+++ tutorial.js
@@ -0,0 +1,380 @@
+export const Tutorials = {
+    name: "Tutorials",
+    HTMLandCSS:{
+        name:"HTML and CSS",
+        menu:[
+            {
+                title:"Learn HTML",
+                link:"https://www.w3schools.com/html/default.asp"
+            },
+            {
+                title:"Learn CSS",
+                link:"#"
+            },
+            {
+                title:"Learn RWD",
+                link:"#"
+            },
+            {
+                title:"Learn Bootstrap",
+                link:"#"
+            },
+            {
+                title:"Learn Colors",
+                link:"#"
+            },
+            {
+                title:"Learn Icon",
+                link:"#"
+            },
+            {
+                title:"Learn Graphics",
+                link:"#"
+            },
+            {
+                title:"Learn SVG",
+                link:"#"
+            },
+            {
+                title:"Learn Canvas",
+                link:"#"
+            },
+            {
+                title:"Learn How To",
+                link:"#"
+            },
+            {
+                title:"Learn Saas",
+                link:"#"
+            },
+            {
+                title:"Learn Tailwind",
+                link:"#"
+            },
+        ]
+    },
+    JavaScript:{
+        name:"JavaScript",
+        menu:[
+
+                {
+                    title:"Learn JavaScript",
+                    link:"#"
+                },
+                {
+                    title:"Learn JQuery",
+                    link:"#"
+                },
+                {
+                    title:"Learn React",
+                    link:"#"
+                },
+                {
+                    title:"Learn AngularJS",
+                    link:"#"
+                },
+                {
+                    title:"Learn JSON",
+                    link:"#"
+                },
+                {
+                    title:"Learn AJAX",
+                    link:"#"
+                },
+                {
+                    title:"Learn AppML",
+                    link:"#"
+                },
+                {
+                    title:"Learn CC.JS",
+                    link:"#"
+                },
+            
+        ]
+    },
+    ServerSide:{
+        name:"Server Side",
+        menu:[
+            {
+                title:"Learn Node.js",
+                link:"#"
+            },
+            {
+                title:"Learn Express.js",
+                link:"#"
+            },
+            {
+                title:"Learn MongoDB",
+                link:"#"
+            },
+            {
+                title:"Learn MySQL",
+                link:"#"
+            },
+            {
+                title:"Learn PostgreSQL",
+                link:"#"
+            },
+            {
+                title:"Learn Firebase",
+                link:"#"
+            },
+            {
+                title:"Learn AWS",
+                link:"#"
+            },
+            {
+                title:"Learn GCP",
+                link:"#"
+            },
+            {
+                title:"Learn Docker",
+                link:"#"
+            },
+            {
+                title:"Learn Kubernetes",
+                link:"#"
+            },
+            {
+                title:"Learn Terraform",
+                link:"#"
+            },
+            {
+                title:"Learn AWS Lambda",
+                link:"#"
+            },
+            {
+                title:"Learn AWS API Gateway",
+                link:"#"
+            },
+        ]
+            
+    },
+    DataAnalyst:{
+        name:"Data Analyst",
+        menu:[
+            {
+                title:"Learn SQL",
+                link:"#"
+            },
+            {
+                title:"Learn NoSQL",
+                link:"#"
+            },
+            {
+                title:"Learn Tableau",
+                link:"#"
+            },
+            {
+                title:"Learn Power BI",
+                link:"#"
+            },
+            {
+                title:"Learn QlikView",
+                link:"#"
+            },
+            {
+                title:"Learn Power Query",
+                link:"#"
+            },
+            {
+                title:"Learn Data Visualization",
+                link:"#"
+            },
+            {
+                title:"Learn Data Science",
+                link:"#"
+            },
+            {
+                title:"Learn Machine Learning",
+                link:"#"
+            },
+            {
+                title:"Learn Deep Learning",
+                link:"#"
+            },
+            {
+                title:"Learn Natural Language Processing",
+                link:"#"
+            },
+            {
+                title:"Learn Computer Vision",
+                link:"#"
+            },
+            {
+                title:"Learn Blockchain",
+    },
+]
+    },
+Programming:{
+    name:"Programming",
+    menu:[
+        {
+            title:"Learn C++",
+            link:"#"
+        },
+        {
+            title:"Learn Java",
+            link:"#"
+        },
+        {
+            title:"Learn Python",
+            link:"#"
+        },
+        {
+            title:"Learn Ruby",
+            link:"#"
+        },
+        {
+            title:"Learn Go",
+            link:"#"
+        },
+        {
+            title:"Learn Rust",
+            link:"#"
+        },
+        {
+            title:"Learn Scala",
+            link:"#"
+        },
+        {
+            title:"Learn Kotlin",
+            link:"#"
+        },
+        {
+            title:"Learn Swift",
+            link:"#"
+        },
+        {
+            title:"Learn Assembly Language",
+            link:"#"
+        },
+        {
+            title:"Learn Fortran",
+            link:"#"
+        },
+        {
+            title:"Learn Haskell",
+            link:"#"
+        },
+        {
+            title:"Learn Prolog",
+            link:"#"
+        },
+        
+    ]
+},
+WebBuilding:{
+    name:"Web Building",
+    menu:[
+        {
+            title:"Learn HTML",
+            link:"#"
+        },
+        {
+            title:"Learn CSS",
+            link:"#"
+        },
+        {
+            title:"Learn SASS",
+            link:"#"
+        },
+        {
+            title:"Learn LESS",
+            link:"#"
+        },
+        {
+            title:"Learn Bootstrap",
+            link:"#"
+        },
+        {
+            title:"Learn Foundation",
+            link:"#"
+        },
+        {
+            title:"Learn Materialize",
+            link:"#"
+        },
+        {
+            title:"Learn Semantic UI",
+            link:"#"
+        },
+        {
+            title:"Learn Tailwind CSS",
+            link:"#"
+        },
+        {
+            title:"Learn Bulma CSS",
+            link:"#"
+        },
+        {
+            title:"Learn Ant Design",
+            link:"#"
+        },
+        {
+            title:"Learn React",
+            link:"#"
+        },
+        {
+            title:"Learn Vue.js",
+            link:"#"
+},
+    ]
+},
+XMLTutorials:{
+    name:"XML Tutorials",
+    menu:[
+        {
+            title:"Learn XML",
+            link:"#"
+        },
+        {
+            title:"Learn XSLT",
+            link:"#"
+        },
+        {
+            title:"Learn XPath",
+            link:"#"
+        },
+        {
+            title:"Learn JSON",
+            link:"#"
+        },
+        {
+            title:"Learn SOAP",
+            link:"#"
+        },
+        {
+            title:"Learn RESTful API",
+            link:"#"
+        },
+        {
+            title:"Learn XML Schema",
+            link:"#"
+        },
+        {
+            title:"Learn DTD",
+            link:"#"
+        },
+        {
+            title:"Learn Schematron",
+            link:"#"
+        },
+        {
+            title:"Learn XQuery",
+            link:"#"
+        },
+        {
+            title:"Learn XSLT 2.0",
+            link:"#"
+        },
+        {
+            title:"Learn XSLT 3.0",
+            link:"#"
+        },
+    ]
+}
+}
+
+
+
